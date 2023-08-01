49a2014af648d17f81f46b2e63140c6d96e2ef43
Yoel Hor <yoelhor@live.com>
2023-08-01 12:00:51 +0300

    Angular Tabs placehhodr

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { OverlayModule } from "@angular/cdk/overlay";
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -28,6 +31,9 @@ import { NavMenuLoginComponent } from './nav-menu-login/nav-menu-login.component
     HttpClientModule,
     FormsModule,
     OverlayModule,
+    MatIconModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
