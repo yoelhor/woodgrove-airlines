aa5a205000976c88180327fbb0715dd4a2c7744b
Yoel Hor <yoelhor@live.com>
2023-08-01 18:53:38 +0300

    CIAM call to initiate endpoint

--- ClientApp/src/app/nav-menu-login/nav-menu-login.component.ts
+++ ClientApp/src/app/nav-menu-login/nav-menu-login.component.ts
@@ -1,5 +1,5 @@
-import { Component, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, Inject, ViewChild } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-nav-menu-login',
@@ -7,16 +7,22 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./nav-menu-login.component.css']
 })
 export class NavMenuLoginComponent {
-  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) 
-  { 
-    this.baseUrl = baseUrl
-  }
-  isOpen = false;
-  baseUrl = "";
+  constructor(private http: HttpClient) {}
 
-  Login() {
-    console.log(this.baseUrl);
-    this.http.get(this.baseUrl + 'weatherforecast').subscribe(result => {
+  @ViewChild('signInEmail') signInEmail!: ElementRef;
+  
+  isOpen = false;
+  baseUrl = "https://wggdev.ciamlogin.com/wggdev.onmicrosoft.com/oauth2/v2.0/";
+  appID = "f28eb79b-f279-45ca-87b1-6a8421f54bc1"
+
+  LoginInitiate() {
+
+    const formData = new FormData();
+    formData.append('client_id', this.appID);
+    formData.append('challenge_type', 'password redirect');
+    formData.append('username', this.signInEmail.nativeElement.value);
+
+    this.http.post(this.baseUrl + 'initiate?dc=ESTS-PUB-WUS2-AZ1-FD000-TEST1', formData).subscribe(result => {
       console.log(result);
       this.isOpen = false;
     }, error => console.error(error));
